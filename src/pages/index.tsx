16c8a0cbe23ec31c07d7202e8039d3f7cf4a3e98
Felipe Stein Rosa <projectstein@gmail.com>
2022-04-11 14:23:52 -0300

    feat: mapping pricing box

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,8 @@ const Index = ({
   sectionTech,
   sectionConcepts,
   sectionModules,
-  sectionAgenda
+  sectionAgenda,
+  pricingBox
 }: LandingPageProps) => (
   <>
     <SectionHero logo={logo} header={header} />
@@ -32,7 +33,7 @@ const Index = ({
     <SectionConcepts {...sectionConcepts} />
     <SectionModules {...sectionModules} />
     <SectionAgenda {...sectionAgenda} />
-    <PricingBox />
+    <PricingBox {...pricingBox} />
     <SectionAboutUs />
     <SectionReviews />
     <SectionFaq />
